b56b1e7de69df496d442e876c27e0eac22a3b0b0
Pablo Barreyro <Pabarreyro@gmail.com>
2018-06-01 11:23:44 -0700

    Add test for proper Team instantiation

--- src/test/java/TeamTest.java
+++ src/test/java/TeamTest.java
@@ -1,5 +1,13 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class TeamTest {
-
+    @Test
+    public void newTeam_instantiatesProperly_teamDetails() throws Exception{
+        Team testTeam = new Team("TestTeam", "John Doe", "Test Description");
+        assertEquals("TestTeam", testTeam.getName());
+        assertEquals("John Doe", testTeam.getMembers().get(0));
+        assertEquals("Test Description", testTeam.getProduct());
+    }
 }
