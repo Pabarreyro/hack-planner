17fd1591da5cb00d380427312d7fdd6b4dddf08e
Pablo Barreyro <Pabarreyro@gmail.com>
2018-06-01 11:24:38 -0700

    Add passing logic for instantiating and retrieving Team information

--- src/main/java/Team.java
+++ src/main/java/Team.java
@@ -1,3 +1,27 @@
-public class Team {
+import java.util.ArrayList;
 
+public class Team {
+    private String name;
+    private ArrayList<String> members;
+    private String product;
+
+    public Team(String name, String members, String product) {
+        this.name = name;
+        this.members = new ArrayList<String>(){{
+            add(members);
+        }};
+        this.product = product;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<String> getMembers() {
+        return members;
+    }
+
+    public String getProduct() {
+        return product;
+    }
 }
