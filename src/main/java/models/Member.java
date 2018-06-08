06155c4662b16d80f3053378d0c4a1720e1bc3b0
Pablo Barreyro <Pabarreyro@gmail.com>
2018-06-08 12:03:54 -0700

    Remove instances static, add getters and setters

--- src/main/java/models/Member.java
+++ src/main/java/models/Member.java
@@ -1,4 +1,27 @@
 package models;
 
+import java.util.ArrayList;
+
 public class Member {
+    private String name;
+    private Team team;
+    private int id;
+
+    public Member(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {  this.name = name; }
+
+    public Team getTeam() { return team; }
+
+    public void setTeam(Team team) { this.team = team; }
+
+    public int getId() { return id; }
+
+    public void setId(int id) { this.id = id; }
 }
