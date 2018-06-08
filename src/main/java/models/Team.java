982f0c052117a4dcb1421419ea992c06523595f9
Pablo Barreyro <Pabarreyro@gmail.com>
2018-06-08 12:01:06 -0700

    Remove members field and instances static

--- src/main/java/models/Team.java
+++ src/main/java/models/Team.java
@@ -1,38 +1,23 @@
 package models;
 
-import java.util.ArrayList;
-
 public class Team {
     private String name;
-    private ArrayList<String> members;
+    private int memberCount;
     private String product;
-    private static ArrayList<Team> teams = new ArrayList<>();
     private int id;
 
-    public Team(String name, String members, String product) {
+    public Team(String name, String product) {
         this.name = name;
-        this.members = new ArrayList<String>(){{
-            add(members);
-        }};
         this.product = product;
-        teams.add(this);
-        this.id = teams.size();
+        this.memberCount = 0;
     }
 
     public String getName() {
         return name;
     }
 
-    public ArrayList<String> getMembers() {
-        return members;
-    }
-
     public String getProduct() {
         return product;
-    }
-
-    public void setMembers(String memberName) {
-        this.members.add(memberName);
     }
 
     public void setName(String teamName) {
@@ -43,19 +28,11 @@ public class Team {
         this.product = product;
     }
 
-    public static ArrayList<Team> getAll() {
-        return teams;
-    }
-
-    public static void clearAll() {
-        teams.clear();
-    }
-
     public int getId() {
         return this.id;
     }
 
-    public static Team findById(int id) {
-        return teams.get(id - 1);
-    }
+    public int getMemberCount() { return memberCount; }
+
+    public void setMemberCount(int memberCount) { this.memberCount = memberCount; }
 }
