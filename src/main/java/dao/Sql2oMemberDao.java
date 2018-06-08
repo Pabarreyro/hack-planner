96d8b07ef99c017649d2608d2172992ba7adc514
Pablo Barreyro <Pabarreyro@gmail.com>
2018-06-08 13:08:07 -0700

    Add constructor

--- src/main/java/dao/Sql2oMemberDao.java
+++ src/main/java/dao/Sql2oMemberDao.java
@@ -1,5 +1,9 @@
 package dao;
 
-public class Sql2oMemberDao implements MemberDao {
+import org.sql2o.*;
 
+public class Sql2oMemberDao implements MemberDao {
+    private final Sql2o sql2o;
+
+    public Sql2oMemberDao(Sql2o sql2o) { this.sql2o = sql2o; }
 }
--- src/main/java/dao/Sql2oTeamDao.java
+++ src/main/java/dao/Sql2oTeamDao.java
@@ -1,5 +1,9 @@
 package dao;
 
-public class Sql2oTeamDao implements TeamDao{
+import org.sql2o.*;
 
+public class Sql2oTeamDao implements TeamDao{
+    private final Sql2o sql2o;
+
+    public Sql2oTeamDao(Sql2o sql2o) { this.sql2o = sql2o; }
 }
