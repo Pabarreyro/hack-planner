52feab43d7d1e1407f86ba3bbdcd186b19b6ce64
Pablo Barreyro <Pabarreyro@gmail.com>
2018-06-08 14:10:18 -0700

    Add add() logic

--- src/main/java/dao/Sql2oMemberDao.java
+++ src/main/java/dao/Sql2oMemberDao.java
@@ -1,9 +1,26 @@
 package dao;
 
+import models.Member;
 import org.sql2o.*;
+
+import java.util.List;
 
 public class Sql2oMemberDao implements MemberDao {
     private final Sql2o sql2o;
 
     public Sql2oMemberDao(Sql2o sql2o) { this.sql2o = sql2o; }
+
+    @Override
+    public void add(Member member) {
+        String sql = "INSERT into members (name, teamId) VALUES (:name, :teamId)";
+        try (Connection con = sql2o.open()) {
+            int id = (int) con.createQuery(sql, true)
+                    .bind(member)
+                    .executeUpdate()
+                    .getKey();
+            member.setId(id);
+        }  catch (Sql2oException ex) {
+            System.out.println(ex);
+        }
+    }
 }
--- src/main/java/dao/Sql2oTeamDao.java
+++ src/main/java/dao/Sql2oTeamDao.java
@@ -1,9 +1,26 @@
 package dao;
 
+import models.Team;
 import org.sql2o.*;
+
+import java.util.List;
+import java.util.Locale;
 
 public class Sql2oTeamDao implements TeamDao{
     private final Sql2o sql2o;
 
     public Sql2oTeamDao(Sql2o sql2o) { this.sql2o = sql2o; }
+
+    @Override
+    public void add(Team team) {
+        String sql = "INSERT into teams (name, memberCount, product) VALUES (:name, :memberCount, :product)";
+        try (Connection con = sql2o.open()) {
+            int id = (int) con.createQuery(sql, true).bind(team).executeUpdate().getKey();
+            team.setId(id);
+        } catch (Sql2oException ex) {
+            System.out.println(ex);
+        }
+    }
+
+
 }
