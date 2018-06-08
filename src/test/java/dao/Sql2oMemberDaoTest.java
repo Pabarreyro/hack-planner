2e0c65a98b32d38797dcacab002c54ac64505bf3
Pablo Barreyro <Pabarreyro@gmail.com>
2018-06-08 13:07:18 -0700

    Add setUp() and tearDown() logic

--- src/test/java/dao/Sql2oMemberDaoTest.java
+++ src/test/java/dao/Sql2oMemberDaoTest.java
@@ -1,17 +1,26 @@
 package dao;
 
-import org.junit.After;
-import org.junit.Before;
+import org.junit.*;
+import org.sql2o.*;
 
 import static org.junit.Assert.*;
 
 public class Sql2oMemberDaoTest {
+    private Sql2oTeamDao teamDao;
+    private Sql2oMemberDao memberDao;
+    private Connection conn;
 
     @Before
     public void setUp() throws Exception {
+        String connectionString = "jdbc:h2:mem:testing;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
+        Sql2o sql2o = new Sql2o(connectionString, "", "");
+        teamDao = new Sql2oTeamDao(sql2o);
+        memberDao = new Sql2oMemberDao(sql2o);
+        conn = sql2o.open();
     }
 
     @After
     public void tearDown() throws Exception {
+        conn.close();
     }
 }
